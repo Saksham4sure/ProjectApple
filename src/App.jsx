c1bf6d0bc0abbc75d010e1c4eca1d6ab1a6fc39e
Saksham Shrestha <saksham@Sakshams-MacBook-Pro.local>
2025-06-08 20:33:28 +0545

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Landingpage from './components/Landingpage'
 import Secondpage from './components/Secondpage'
 import Thirdhero from './components/Thirdhero'
 import Promopage from './components/Promopage'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return (git add .
+
     <div>
       
       <Navbar/>
